e73c53334199084147778539e796040e9ef07f96
404OS <18780235193@163.com>
2023-04-02 13:36:27 +0300

    for test

--- kernel_about/include/kernel.h
+++ kernel_about/include/kernel.h
@@ -1,2 +1,12 @@
+#ifndef _KERNEL_H__
+#define _KERNEL_H__
+
 #include<platform.h>
 #include<uart.h>
+
+typedef unsigned char u8;			//8bit
+typedef unsigned short u16;		//16bit
+typedef unsigned int u32;			//32bit
+typedef unsigned long u64;		//64bit
+
+#endif
