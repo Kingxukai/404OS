93d3694a1d65bc0783033f22cfb991fc57fb2096
404OS <18780235193@163.com>
2023-05-03 21:35:16 +0800

    add syscall arg num from 0-6

--- include/system.h
+++ include/system.h
@@ -66,4 +66,49 @@ type name(type0 name0,type1 name1,type2 name2) \
 	return ret; \
 }
 
+#define _syscall4(type,name,type0,name0,type1,name1,type2,name2,type3,name3) \
+type name(type0 name0,type1 name1,type2 name2) \
+{ \
+	asm volatile("li a7, %[_nr]" ::[_nr] "i" (NR_##name)); \
+  asm volatile("mv a0, %[arg0]" ::[arg0] "r" ((type0)name0)); \
+  asm volatile("mv a1, %[arg1]" ::[arg1] "r" ((type1)name1)); \
+  asm volatile("mv a2, %[arg2]" ::[arg2] "r" ((type2)name2)); \
+  asm volatile("mv a3, %[arg3]" ::[arg3] "r" ((type3)name3)); \
+	asm volatile("ecall"); \
+	type ret; \
+	asm volatile("mv %[arg4],a0":[arg4]"=r" ((type)ret)); \
+	return ret; \
+}
+
+#define _syscall5(type,name,type0,name0,type1,name1,type2,name2,type3,name3,type4,name4) \
+type name(type0 name0,type1 name1,type2 name2) \
+{ \
+	asm volatile("li a7, %[_nr]" ::[_nr] "i" (NR_##name)); \
+  asm volatile("mv a0, %[arg0]" ::[arg0] "r" ((type0)name0)); \
+  asm volatile("mv a1, %[arg1]" ::[arg1] "r" ((type1)name1)); \
+  asm volatile("mv a2, %[arg2]" ::[arg2] "r" ((type2)name2)); \
+  asm volatile("mv a3, %[arg3]" ::[arg3] "r" ((type3)name3)); \
+  asm volatile("mv a4, %[arg4]" ::[arg4] "r" ((type4)name4)); \
+	asm volatile("ecall"); \
+	type ret; \
+	asm volatile("mv %[arg5],a0":[arg5]"=r" ((type)ret)); \
+	return ret; \
+}
+
+#define _syscall6(type,name,type0,name0,type1,name1,type2,name2,type3,name3,type4,name4,type5,name5) \
+type name(type0 name0,type1 name1,type2 name2) \
+{ \
+	asm volatile("li a7, %[_nr]" ::[_nr] "i" (NR_##name)); \
+  asm volatile("mv a0, %[arg0]" ::[arg0] "r" ((type0)name0)); \
+  asm volatile("mv a1, %[arg1]" ::[arg1] "r" ((type1)name1)); \
+  asm volatile("mv a2, %[arg2]" ::[arg2] "r" ((type2)name2)); \
+  asm volatile("mv a3, %[arg3]" ::[arg3] "r" ((type3)name3)); \
+  asm volatile("mv a4, %[arg4]" ::[arg4] "r" ((type4)name4)); \
+  asm volatile("mv a5, %[arg5]" ::[arg5] "r" ((type5)name5)); \
+	asm volatile("ecall"); \
+	type ret; \
+	asm volatile("mv %[arg6],a0":[arg6]"=r" ((type)ret)); \
+	return ret; \
+}
+
 #endif
