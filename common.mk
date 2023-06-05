1f5efb02a9c54faa71614798d6ea94be64fb9df0
wxk <18780235193@163.com>
2023-06-05 14:05:44 +0800

    add S-mode

--- common.mk
+++ common.mk
@@ -3,7 +3,7 @@ CFLAGS = -nostdlib -fno-builtin -march=rv64ima -mabi=lp64 -mcmodel=medany -g -Wa
 
 QEMU = qemu-system-riscv64
 QFLAGS = -nographic -smp 1 -machine virt -bios default -m 128M \
--drive file=../sdcard.img,if=none,format=raw,id=x0 \
+-drive file=sdcard.img,if=none,format=raw,id=x0 \
 -device virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0 \
 -global virtio-mmio.force-legacy=false
 
