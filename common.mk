ede1ce8cb7e28fa55c7dfbe2a21024b266f173cf
wxk <18780235193@163.com>
2023-06-07 18:05:16 +0800

    test

--- common.mk
+++ common.mk
@@ -2,7 +2,7 @@ CROSS_COMPILE = riscv64-unknown-elf-
 CFLAGS = -nostdlib -fno-builtin -march=rv64ima -mabi=lp64 -mcmodel=medany -g -Wall -Wno-unused-but-set-variable
 
 QEMU = qemu-system-riscv64
-QFLAGS = -nographic -smp 1 -machine virt -bios default -m 128M \
+QFLAGS = -nographic -smp 2 -machine virt -bios default -m 128M \
 -drive file=sdcard.img,if=none,format=raw,id=x0 \
 -device virtio-blk-device,drive=x0,bus=virtio-mmio-bus.0 \
 -global virtio-mmio.force-legacy=false
